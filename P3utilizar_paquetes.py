dbbe31610771b06cb8c99bb2dc3f1d5cfce54ef8
jadriansolu-alt <jadriansolu@gmail.com>
2026-06-14 14:43:04 -0600

    Add files via upload

---
+++ P3utilizar_paquetes.py
@@ -0,0 +1,16 @@
+print("\033c")
+from paquete1 import Ejercicio1_modulos, modulo_paquete
+
+
+
+Ejercicio1_modulos.borrar
+Ejercicio1_modulos.funcion1()
+nom="Daniel"
+ape="Carreon"
+
+
+
+name,lastname=Ejercicio1_modulos.funcion4(nom,ape)
+men=modulo_paquete.funcion4(nom,ape)
+print(f"name;{name}\n Last Name: {lastname}")
+print(men)
